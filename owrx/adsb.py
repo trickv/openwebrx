7d87f103edbd5785c2c28bf31bfca7d642afd94f
Marat Fayzullin <luarvique@gmail.com>
2023-02-06 16:50:59 -0500

    Trying to make it work.

--- owrx/adsb.py
+++ owrx/adsb.py
@@ -1,4 +1,4 @@
-from owrx.audio import AudioChopperProfile, ConfigWiredProfileSource
+from owrx.audio import AudioChopperProfile, StaticProfileSource
 from owrx.audio.chopper import AudioChopperParser
 import re
 from owrx.map import Map, LocatorLocation
@@ -15,29 +15,22 @@ logger = logging.getLogger(__name__)
 
 
 class AdsbProfile(AudioChopperProfile, metaclass=ABCMeta):
+    def getInterval(self):
+        return 60
+
     def getFileTimestampFormat(self):
         return "%y%m%d_%H%M%S"
 
     def decoder_commandline(self, file):
-        return ["dump1090-mutability", "--ifile", "-", "--iformat", "SC16", ">", file]
+        return ["dump1090-mutability", "--ifile", file, "--iformat", "SC16"]
 
-    @abstractmethod
-    def get_sub_mode(self):
-        pass
+    def getMode(self):
+        return "ADSB"
 
 
-class AdsbProfileSource(ConfigWiredProfileSource):
-    def getPropertiesToWire(self) -> List[str]:
-        return ["adsb_enabled_profiles"]
-
-    def getProfiles(self) -> List[AudioChopperProfile]:
-        config = Config.get()
-        profiles = config["adsb_enabled_profiles"] if "adsb_enabled_profiles" in config else []
-        return [self._loadProfile(p) for p in profiles]
-
-    def _loadProfile(self, profileName):
-        className = "AdsbProfile"
-        return globals()[className]()
+class AdsbProfileSource(StaticProfileSource):
+    def __init__(self):
+        super().__init__([AdsbProfile()])
 
 
 class AdsbParser(AudioChopperParser):
