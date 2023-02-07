3adc36a7e5adce575688582dab6f2d89cfea3216
Marat Fayzullin <luarvique@gmail.com>
2023-02-06 20:48:35 -0500

    Adding SSTV decoder.

--- owrx/modes.py
+++ owrx/modes.py
@@ -141,6 +141,7 @@ class Modes(object):
         DigitalMode("cwdecoder", "CWDecoder", underlying=["usb"]),
         DigitalMode("rtty170", "RTTY-170", underlying=["usb"]),
         DigitalMode("rtty450", "RTTY-450", underlying=["usb"]),
+        DigitalMode("sstv", "SSTV", underlying=["usb"]),
     ]
 
     @staticmethod
